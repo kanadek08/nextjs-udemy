1746069bb96da9d9d8f435f2399bd1017072e14f
纐纈翔奏 <kouketsukanade@MacBook-Pro.local>
2023-11-05 01:34:43 +0900

    all done

---
+++ components/Layout.js
@@ -0,0 +1,46 @@
+import Head from "next/head";
+import styles from "./layout.module.css";
+import utilStyles from "../styles/utils.module.css";
+import Link from "next/link";
+
+const name = "kanadek08";
+export const siteTitle = "Next.js Sample Website";
+
+function Layout ({children, home}) {
+  return (
+    <div>
+      <Head>
+        <title>Next.js Blog</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <header className={styles.header}>
+        {home ? (
+          <>
+            <img 
+              src="/images/profile.png"
+              className={`${styles.headerHomeImage} ${utilStyles.borderCircle}`}
+            />
+            <h1 className={utilStyles.heading2xl}>{name}</h1>
+          </>
+        ) : (
+          <>
+            <img 
+            src="/images/profile.png"
+            className={`${styles.headerImage} ${utilStyles.borderCircle}`}
+            />
+          </>
+        )}
+      </header>
+      <main>{children}</main>
+      {!home && (
+        <div className={styles.backToHome}>
+          <Link href="/">
+            <a>← ホームへ戻る</a>
+          </Link>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default Layout;
---
+++ pages/index.js
@@ -0,0 +1,51 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from 'Styles/Home.module.css'
+import Link from 'next/link'
+import Layout, { siteTitle } from '/components/Layout'
+import utilStyles from 'Styles/utils.module.css'
+import {getPostsData} from 'lib/posts'
+
+export async function getStaticProps() {
+  const allPostsData = getPostsData();
+  console.log(allPostsData);
+  return {
+    props: {
+      allPostsData,
+    }
+  }
+
+}
+export default function Home({ allPostsData }) {
+  return (
+
+    <main className={styles.main}>
+      <Head>
+        <title>{siteTitle}</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Layout home>
+        <section>
+          <p className={utilStyles.headingMd}>20231022からReact勉強中、現在22.5時間。</p>
+        </section>
+        <section>
+          <h2 className={utilStyles.headingLg}>📝 React勉強日記</h2>
+          <div className={styles.grid}>
+            {allPostsData.map(({id, title, date, thumbnail}) => (
+              <article key={id}>
+                <Link href={`/posts/${id}`}>
+                  <img src={`${thumbnail}`} className={styles.thumbnailImage}></img>
+                </Link>
+                <Link href={`/posts/${id}`}>
+                  <p className={utilStyles.boldText}>{title}</p>
+                </Link>
+                <small className={utilStyles.lightText}>{date}</small>
+              </article>
+            ))}
+          </div>
+        </section>
+      </Layout>
+    </main>
+  )
+}
